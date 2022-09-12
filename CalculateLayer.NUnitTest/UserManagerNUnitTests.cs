6d01b74b9d8c2d2dc46e07ce9e3137d5f6a6fbec
mhmd7776 <mhmd7776@yahoo.com>
2022-09-12 23:41:02 +0430

    Add User Manager Test Class

---
+++ CalculateLayer.NUnitTest/UserManagerNUnitTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+namespace CalculateLayer.NUnitTest
+{
+    [TestFixture]
+    public class UserManagerNUnitTests
+    {
+        // Arrange
+        private UserManager _userManager;
+
+        [SetUp]
+        public void Initializer()
+        {
+            _userManager = new UserManager();
+        }
+
+        [Test]
+        public void GetFullName_InputFirstAndLastName_OutputFullName()
+        {
+            // Act
+            var fullName = _userManager.GetFullName("Mohammad", "Mahdavi");
+
+            // Assert
+            Assert.That(fullName, Is.EqualTo("Mohammad Mahdavi"));
+            Assert.That(fullName, Does.Contain(" "));
+            Assert.That(fullName, Does.StartWith("mohammad").IgnoreCase);
+            Assert.That(fullName, Does.Match("[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]"));
+        }
+    }
+}
---
+++ CalculateLayer/UserManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateLayer
+{
+    public class UserManager
+    {
+        public string GetFullName(string name, string family)
+        {
+            return $"{name} {family}";
+        }
+    }
+}
